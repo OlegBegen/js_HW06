e58f6740dc8660df7acadab1e9919c1bbb8107d9
OlegBegen <40671741+OlegBegen@users.noreply.github.com>
2022-11-28 20:20:36 +0200

    fix 10

--- js_HW06_10.js
+++ js_HW06_10.js
@@ -10,20 +10,26 @@ function getRandomHexColor() {
    buttonDestoyEl: document.querySelector("[data-destroy]"), 
    boxesEl: document.querySelector("#boxes"), 
  }; 
- const { inputEl, buttonCreateEl, buttonDestoyEl, boxesEl } = elements; 
+ const {inputEl, buttonCreateEl, buttonDestoyEl, boxesEl} = elements; 
   
- const destroyBoxes = () => boxesEl.innerHTML = "";
-  
- const createBoxes = (amount) => {
-   amount = inputEl.value;
+const destroyBoxes = () => boxesEl.innerHTML = "";
+ 
+ 
+ 
+ const createBoxes = () => {
    const boxArr = [];
+   const amount = inputEl.value;
+   console.log(amount);
+   let boxSize = 30;  
+
 
    for (let i = 0; i < amount; i += 1) {
     const boxEl = document.createElement("div");
-    boxEl.style.width = ${i * 10 + 30} px;   
-    boxEl.style.height = boxEl.style.width;
+     boxEl.style.width = boxSize + "px"; 
+    boxEl.style.height = boxSize + "px";
     boxEl.style.backgroundColor = getRandomHexColor();
-    boxArr.push(boxEl);
+     boxArr.push(boxEl);
+     boxSize += 10;
    };
   
    boxesEl.append(...boxArr);
