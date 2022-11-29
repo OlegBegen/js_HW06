8ac57a5d8bab8e5af86988fbd9203f9d036cd206
OlegBegen <40671741+OlegBegen@users.noreply.github.com>
2022-11-30 00:47:40 +0200

    add29112022

--- js_HW06_10.js
+++ js_HW06_10.js
@@ -4,28 +4,23 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
- const elements = { 
-   inputEl: document.querySelector("input"), 
-   buttonCreateEl: document.querySelector("[data-create]"), 
-   buttonDestoyEl: document.querySelector("[data-destroy]"), 
-   boxesEl: document.querySelector("#boxes"), 
- }; 
- const {inputEl, buttonCreateEl, buttonDestoyEl, boxesEl} = elements; 
+ const elements = {
+   inputEl: document.querySelector("input"),
+   buttonCreateEl: document.querySelector("[data-create]"),
+   buttonDestoyEl: document.querySelector("[data-destroy]"),
+   boxesEl: document.querySelector("#boxes"),
+ };
+ const {inputEl, buttonCreateEl, buttonDestoyEl, boxesEl} = elements;
   
 const destroyBoxes = () => boxesEl.innerHTML = "";
- 
- 
  
  const createBoxes = () => {
    const boxArr = [];
    const amount = inputEl.value;
-   console.log(amount);
-   let boxSize = 30;  
-
-
+   let boxSize = 30;
    for (let i = 0; i < amount; i += 1) {
     const boxEl = document.createElement("div");
-     boxEl.style.width = boxSize + "px"; 
+     boxEl.style.width = boxSize + "px";
     boxEl.style.height = boxSize + "px";
     boxEl.style.backgroundColor = getRandomHexColor();
      boxArr.push(boxEl);
